50266be76cdd84e4c0fd53b8c8452d9bbc0943df
Gideon <g.vdherik@live.nl>
2021-02-03 12:01:13 +0100

    Klein begin gemaakt. Klein voorbeeldje.
    Klein begin gemaakt. Klein voorbeeldje. Ook gelijk database alvast aangemaakt en een dbdiagrom.io databaseje opgezet als demo.

--- includes/datalayer.php
+++ includes/datalayer.php
@@ -1,7 +1,3 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: gvdhe
- * Date: 3-2-2021
- * Time: 11:16
- */
+
+
--- index.php
+++ index.php
@@ -1,7 +1,30 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: gvdhe
- * Date: 3-2-2021
- * Time: 10:10
- */
+require "includes/datalayer.php";
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>To-Do List</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="resources/css/style.css">
+</head>
+<body>
+    <div id="main-container" class="container">
+        <div class="row justify-content-center">
+            <div class="col-3 bg-primary">
+                <div class="title text-center border-bottom border-3 border-danger">
+                    <h2>To-Do List #1</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
